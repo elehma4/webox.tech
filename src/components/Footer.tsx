07d4b46105840241692cd0bcf13f29e720ddbffe
elehmandevelopment <elehman.development@gmail.com>
2024-05-26 14:46:04 -0600

    display: footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,60 +9,36 @@ import logo from "@/images/logos/webox-high-resolution-logo-transparent.png"
 
 const navigation = [
   {
-    title: 'Work',
-    links: [
-      { title: 'FamilyFund', href: '/work/family-fund' },
-      { title: 'Unseal', href: '/work/unseal' },
-      { title: 'Phobia', href: '/work/phobia' },
-      {
-        title: (
-          <>
-            See all <span aria-hidden="true">&rarr;</span>
-          </>
-        ),
-        href: '/work',
-      },
-    ],
-  },
-  {
     title: 'Company',
     links: [
       { title: 'About', href: '/about' },
       { title: 'Process', href: '/process' },
-      { title: 'Blog', href: '/blog' },
-      { title: 'Contact us', href: '/contact' },
+      { title: 'Our work', href: '/blog' },
     ],
-  },
-  {
-    title: 'Connect',
-    links: socialMediaProfiles,
   },
 ]
 
 function Navigation() {
   return (
     <nav>
-      <ul role="list" className="grid grid-cols-2 gap-8 sm:grid-cols-3">
+      <div role="list" className="grid grid-cols-1 gap-8 text-center">
         {navigation.map((section, sectionIndex) => (
-          <li key={sectionIndex}>
-            <div className="font-display text-sm font-semibold tracking-wider text-neutral-950">
-              {section.title}
-            </div>
-            <ul role="list" className="mt-4 text-sm text-neutral-700">
+          <div key={sectionIndex} className=''>
+            <div role="list" className="mt-4 text-sm text-neutral-700 lg:flex">
               {section.links.map((link, linkIndex) => (
-                <li key={linkIndex} className="mt-4">
+                <div key={linkIndex} className="py-1">
                   <Link
                     href={link.href}
-                    className="transition hover:text-neutral-950"
+                    className="transition hover:text-neutral-950 lg:px-8 sm:hover:underline"
                   >
                     {link.title}
                   </Link>
-                </li>
+                </div>
               ))}
-            </ul>
-          </li>
+            </div>
+          </div>
         ))}
-      </ul>
+      </div>
     </nav>
   )
 }
@@ -116,13 +92,11 @@ export function Footer() {
   return (
     <Container as="footer" className="mt-24 w-full sm:mt-32 lg:mt-40">
       <FadeIn>
-        <div className="grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-2">
-          <Navigation />
-        </div>
         <div className="mb-20 mt-24 flex flex-wrap items-end justify-between gap-x-6 gap-y-4 border-t border-neutral-950/10 pt-12">
           <Link href="/" aria-label="Home">
-            <Image src={logo} alt={'WebOx Tech Solutions'} className='w-44 h-auto' />
+            <Image src={logo} alt={'WebOx Tech Solutions'} className='w-44 h-auto my-auto max-sm:py-5' />
           </Link>
+          <Navigation />
           <p className="text-sm text-neutral-700">
             © WebOx Solutions {new Date().getFullYear()}
           </p>
